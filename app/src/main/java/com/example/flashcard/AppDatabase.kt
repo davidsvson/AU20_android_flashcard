d42a290a6ce2769670e7b51c6d7a78ed83d7db93
David Svensson <davidsvson@gmail.com>
2020-11-11 11:30:17 +0100

    removed old code

--- app/src/main/java/com/example/flashcard/AppDatabase.kt
+++ app/src/main/java/com/example/flashcard/AppDatabase.kt
@@ -5,22 +5,6 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-/*
-class Person( var name : String, var age: Int) {
-
-    init {
-        count++
-    }
-
-    companion object {
-         var count : Int  = 0
-
-
-    }
-
-}
-*/
-
 @Database(entities = arrayOf(Word::class), version = 1)
 abstract class AppDatabase : RoomDatabase() {
 
